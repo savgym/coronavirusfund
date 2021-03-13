65a8dd473d42878711515e1919a5a4e80a2c52af
Savvas Gymnopoulos <sngymnopoulos@gmail.com>
2021-03-13 19:15:06 +0200

    first commit

---
+++ gethint.php
@@ -0,0 +1,7 @@
+<?php
+
+$fname = $_REQUEST["fname"];
+echo " $fname ,your message has been send<br>";
+echo"we will reply you as soon as possible";
+
+?>
---
+++ myScript.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+ function ClearFields() {
+
+    document.getElementById("firstName").value = "";
+     document.getElementById("lastName").value = "";
+     document.getElementById("username").value = "";
+     document.getElementById("email").value = "";
+     document.getElementById("address").value = "";
+     document.getElementById("zip").value = "";
+     document.getElementById("cc-name").value = "";
+     document.getElementById("cc-number").value = "";
+     document.getElementById("cc-expiration").value = "";
+     document.getElementById("cc-cvv").value = "";
+	 
+  	 
+
+
+ }
+
+ function ClearFieldsTwo(){
+    document.getElementById("inputName").value = "";
+	document.getElementById("inputLastName").value = "";
+    document.getElementById("inputEmail4").value = "";
+    document.getElementById("inputAddress").value = "";
+    document.getElementById("inputCity").value = "";
+    document.getElementById("inputZip").value = "";
+    document.getElementById("message").value = "";
+
+ }
